6041afefe133d9eaa677334abe6671f564e596ed
Axurj <acerj2468@gmail.com>
2020-07-14 14:45:41 -0700

    Started on the JavaScript, at a half-way point

---
+++ form.js
@@ -0,0 +1,5 @@
+let nameRecieved = document.getElementById("username");
+let passRecieved = document.getElementById("password");
+let numRecieved = document.getElementById("num");
+let dateRecieved = document.getElementById("date");
+nameRecieved.addEventListener("submit", getName);
