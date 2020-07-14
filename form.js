826daf314ced5e2246d49ce55081da4637e2b1d5
Axurj <acerj2468@gmail.com>
2020-07-14 16:00:37 -0700

    Added CSS Styling and Comments

--- form.js
+++ form.js
@@ -1,13 +1,17 @@
+// Gets the values based on the IDs and assigns it to a new variable.
 let formRecieved = document.getElementById("inputForm");
 let passRecieved = document.getElementById("password");
 let numRecieved = document.getElementById("num");
 let dateRecieved = document.getElementById("date");
-formRecieved.addEventListener("submit", getResults);
 let nameShown = document.getElementById("showName");
 let passShown = document.getElementById("showPass");
 let numShown = document.getElementById("showNum");
 let dateShown = document.getElementById("showDate");
+// Attaches an EventListener to the submit input and uses the function 
+// getResults to print out the value from each input tag. 
+formRecieved.addEventListener("submit", getResults);
 
+// This function takes the results from the form and prints it out when the submit input is activated.
 function getResults(event) {
     event.preventDefault();
     nameShown.innerHTML = event.target.nameCase.value;
