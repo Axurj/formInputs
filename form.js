6e853070c8c2233111d9fec1bd539282ccf600db
Axurj <acerj2468@gmail.com>
2020-07-14 15:16:22 -0700

    Finished assignment: inputs now get printed out

--- form.js
+++ form.js
@@ -3,9 +3,15 @@ let passRecieved = document.getElementById("password");
 let numRecieved = document.getElementById("num");
 let dateRecieved = document.getElementById("date");
 formRecieved.addEventListener("submit", getResults);
-let showName = document.getElementById("show");
+let nameShown = document.getElementById("showName");
+let passShown = document.getElementById("showPass");
+let numShown = document.getElementById("showNum");
+let dateShown = document.getElementById("showDate");
 
 function getResults(event) {
     event.preventDefault();
-    showName.innerHTML = event.target.nameCase.value;
+    nameShown.innerHTML = event.target.nameCase.value;
+    passShown.innerHTML = event.target.passCase.value;
+    numShown.innerHTML = event.target.numCase.value;
+    dateShown.innerHTML = event.target.dateCase.value;
 }
